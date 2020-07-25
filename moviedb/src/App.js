347951db1cdfa66d3d74e4a81aeb1a77fe9af3a5
Shivani Reddy <shivanireddy010197@gmail.com>
2020-07-25 13:30:01 -0700

    css file changes

---
+++ moviedb/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+      <h1>Movie Database</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
